3567398da53b770c0d9ff1bf98cf17890f42d084
Warczachowski <wlodek_w@yahoo.de>
2024-01-20 11:04:26 +0100

    update

--- src/ebay/ebay.controller.ts
+++ src/ebay/ebay.controller.ts
@@ -1,67 +1,100 @@
-import { Body, Controller, Get, Header, Param, Post, Query, Req, Res, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Query,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { EbayService } from './ebay.service';
 import { JwtAuthGuard } from 'src/auth/auth.jwtGuard.guard';
 import { Request, Response } from 'express';
-import { createHash } from 'crypto';
 import { env } from 'src/env/env';
 import { ebayProccess } from './notifications/ebay.process.notification';
 import { verifyChalange } from './notifications/ebay.notValidator';
 
 @Controller('ebay')
 export class EbayController {
-    constructor(private readonly service: EbayService) {}
+  constructor(private readonly service: EbayService) {}
 
-    @Get()
-    @UseGuards(JwtAuthGuard)
-    async getEbaySoldOrders() {
-        return await this.service.getEbaySoldOrders();
-    }
+  @Get()
+  @UseGuards(JwtAuthGuard)
+  async getEbaySoldOrders() {
+    return await this.service.getEbaySoldOrders();
+  }
 
-    //get url for user consent
-    @Get('consent')
-    @UseGuards(JwtAuthGuard)
-    async getUserConsent() {
-        return await this.service.getUserConsent();
-    }
+  //get url for user consent
+  @Get('consent')
+  @UseGuards(JwtAuthGuard)
+  async getUserConsent() {
+    return await this.service.getUserConsent();
+  }
 
-    //get automatic access token and refreshtoken after user has consent
-    @Get('redirect/consent')
-    async userEbayAcceptConsent(
-        @Query('state') state: number,
-        @Query('code') code: string,
-        @Query('expires_in') expires_in: number
-         ) {
-            await this.service.getAccessToken(code);
-         }
+  //get automatic access token and refreshtoken after user has consent
+  @Get('redirect/consent')
+  async userEbayAcceptConsent(
+    @Query('state') state: number,
+    @Query('code') code: string,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    @Query('expires_in') expires_in: number,
+  ) {
+    await this.service.getAccessToken(code);
+  }
 
-    @Get('deletion')
-    getDeletionEbayResponse(@Query('challenge_code') challenge: string, @Res() res: Response) {
-        const respo = res;
-     /*   const hash = createHash('sha256');
+  @Get('deletion')
+  getDeletionEbayResponse(
+    @Query('challenge_code') challenge: string,
+    @Res() res: Response,
+  ) {
+    const respo = res;
+    /*   const hash = createHash('sha256');
         hash.update(challenge);
         hash.update(env.ebay_deletion_VerificationToken);
         hash.update(env.ebay_deletion_Link);
         const resHash = hash.digest('hex');
         console.log({"challengeResponse": resHash.toString()});*/
-       
-        res.set('Content-type', 'application/json').json({ challengeResponse: verifyChalange(challenge, env.ebay_deletion_Link, env.ebay_deletion_VerificationToken)});
-      return res;
-       
-    }
-    @Post('deletion')
-    async postDeletionEbay(@Req() req: Request, @Res() res: Response) {
-      const proc = await  ebayProccess(req.body, req.headers['x-ebay-signature'], this.service);
-      return res.status(proc).send();
-    }
-    @Post('notifi')
-    async postNotificationFromEbay(@Req() req: Request, @Res() res: Response) {
-        const proc = await ebayProccess(req.body, req.headers['x-ebay-signature'], this.service);
-        return res.status(proc).send();
-    }
-    @Get('notifi')
-    async getNotificationFromEbay(@Query('challenge_code') challenge: string, @Res() res: Response) {
-        const respo = res;
-        res.set('Content-type', 'application/json').json({ challengeResponse: verifyChalange(challenge, env.ebay_notifi_Link, env.ebay_notifi_VerificationToken)});
-      return res;
-    }
+
+    res.set('Content-type', 'application/json').json({
+      challengeResponse: verifyChalange(
+        challenge,
+        env.ebay_deletion_Link,
+        env.ebay_deletion_VerificationToken,
+      ),
+    });
+    return res;
+  }
+  @Post('deletion')
+  async postDeletionEbay(@Req() req: Request, @Res() res: Response) {
+    const proc = await ebayProccess(
+      req.body,
+      req.headers['x-ebay-signature'],
+      this.service,
+    );
+    return res.status(proc).send();
+  }
+  @Post('notifi')
+  async postNotificationFromEbay(@Req() req: Request, @Res() res: Response) {
+    const proc = await ebayProccess(
+      req.body,
+      req.headers['x-ebay-signature'],
+      this.service,
+    );
+    return res.status(proc).send();
+  }
+  @Get('notifi')
+  async getNotificationFromEbay(
+    @Query('challenge_code') challenge: string,
+    @Res() res: Response,
+  ) {
+    const respo = res;
+    res.set('Content-type', 'application/json').json({
+      challengeResponse: verifyChalange(
+        challenge,
+        env.ebay_notifi_Link,
+        env.ebay_notifi_VerificationToken,
+      ),
+    });
+    return res;
+  }
 }
